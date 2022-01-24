8befcbc19cc7ca3b19b72715d7a1b6c63b5d8dd2
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-01-24 23:49:04 +0800

    Clean and format code properly

--- script.js
+++ script.js
@@ -2,65 +2,71 @@ const player = () => {
     
 };
 
+// Module that contains the array storage of the game board
 const gameBoard = (()=>{
     let boardArray = new Array(9);
-
-    const getField = (number) => {
-        return boardArray[number];
-    }
     
-    return { boardArray, getField };
+    return { boardArray };
 })();
 
+// Module that contains the algorithm for checking the game winner
 const gameController = (() => {
+
     const gameCheck = () => {
+        checkForTie();
+
+        const rows = [[0,1,2], [3,4,5], [6,7,8]];
+        
         if(gameBoard.boardArray[0] === 'X' && 
             gameBoard.boardArray[1] === 'X' && 
             gameBoard.boardArray[2] === 'X'){
-            displayController.boxes.forEach(box => {
-                let index = box.getAttribute('index');
-                if(index == '0' || index == '1' || index == '2'){
-                    box.style.backgroundColor = 'aqua';
-                } 
-            })
-            displayController.declareWinner();
-            return 'X';
+            
+        }
+        else if(gameBoard.boardArray[0] === 'X' && 
+            gameBoard.boardArray[4] === 'X' && 
+            gameBoard.boardArray[8] === 'X'){
+            
+        }
+        
+    };
+
+    const checkForTie = () => {
+        for(let i = 0; i < 9; i++){
+            if(gameBoard.boardArray[i] == null){
+                
+                break;
+            }
         }
     };
 
     return { gameCheck };
 })();
 
-
+// Module that controls every display in the page
 const displayController = (() =>{
-    const boxes = document.querySelectorAll('.child');
-    const modeXBtn = document.querySelector('#mode-x');
-    const modeOBtn = document.querySelector('#mode-o');
+    const boxes = document.querySelectorAll('.box');
+    const markerX = document.querySelector('#marker-x');
+    const markerO = document.querySelector('#marker-o');
     const restartBtn = document.querySelector('#restart-btn');
-    const popupContainer = document.querySelector('#popUp-container');
-    const playAgainBtn = document.querySelector('#play-again');
-
     
     boxes.forEach(box => box.addEventListener('click', placeMarker));
     restartBtn.addEventListener('click', clearBoard);
-    playAgainBtn.addEventListener('click', playAgain);
 
     let marker = 'X';
     
     function modeX(){
-        modeOBtn.classList.remove('active');
-        modeXBtn.classList.add('active');
+        markerO.classList.remove('active');
+        markerX.classList.add('active');
         marker = 'X';
     }
 
     function modeO(){
-        modeXBtn.classList.remove('active');
-        modeOBtn.classList.add('active');
+        markerX.classList.remove('active');
+        markerO.classList.add('active');
         marker = 'O';
     }
     
     function placeMarker(e){
-        if(gameController.gameCheck() === 'X') return;
         if(e.target.textContent !== '') return;
         let index = e.target.getAttribute('index');
         gameBoard.boardArray[index] = marker;
@@ -84,17 +90,8 @@ const displayController = (() =>{
         renderContent();
         modeX();
     }
-
-    function declareWinner(){
-        popupContainer.style.display = 'flex';
-    }
-
-    function playAgain(){
-        clearBoard();
-        popupContainer.style.display = 'none';
-    }
     
-    return { boxes, declareWinner };
+    return { boxes };
 })();
 
 
